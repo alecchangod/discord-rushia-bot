ecf355984eeee1ab365bf3bf5adfb9c97c63de28
alecchangod <alec3d02@gmail.com>
2022-11-03 15:38:05 +0800

    removed useless var

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ const { QuickDB } = require("quick.db");
 const db = new QuickDB({ filePath: "database/track.sqlite" });
 
 (async () => {
-const aa = await db.push('track', "949153367609987124");
-console.log(aa)}
+await db.deleteAll
+}
 )()
