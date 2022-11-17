2f28b43b5c9dd33b42a7e525ea347ddf18d87e21
alecchangod <alec3d02@gmail.com>
2022-11-17 21:18:25 +0800

    using db to track roles changes now"

--- Commands/info/mupdate.js
+++ Commands/info/mupdate.js
@@ -1,26 +1,23 @@
-// const { QuickDB } = require("quick.db");
-// const db = new QuickDB({ filePath: "database/roles.sqlite" });
+const { QuickDB } = require("quick.db");
+const db = new QuickDB({ filePath: "database/roles.sqlite" });
 module.exports = {
   name: "mupdate",
   aliases: ["m"],
   run: async (client, oldMember, newMember) => {
 
-    // newMember.roles.cache.forEach(async r => {
-    //   console.log(r.name, r.id)
-    //   const oldr = await db.get(newMember.user.id)
-    //   if(oldr === null || oldr === undefined) {db.push(newMember.id, r.id)}
-    //   else if (JSON.stringify(oldr).includes(r.id) === false) { 
-    //     db.push(newMember.id, r.id) 
-    //     console.log(`${newMember.displayName} was added to ${r.name}`)
-    //   }
-    //   db.set(r.id, r.name)
-    // });
+    const oldr = await db.get(newMember.user.id)
+      if(oldr === null || oldr === undefined){
+    const r = newMember.roles.cache
+    r.forEach(async r => {
+      console.log(r.name, r.id);
+      await db.push(newMember.user.id, r.id)
+    })}
 
     const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
     if (removedRoles.size > 0) {
       console.log(removedRoles.map(r => r.name).toString())
       if ((removedRoles.map(r => r.name).toString().length === 0) || (!removedRoles.map(r => r.name))) return;
-      db.pull(newMember.displayName, removedRoles.map(r => r.id));
+      db.pull(newMember.user.id, removedRoles.map(r => r.id));
       const newr = (`${oldMember.displayName} 不再是 ${removedRoles.map(r => r.name)} 了`);
       console.log(newr);
       const Embed = (`身份組變了欸~\n\n${newr}`)
@@ -34,7 +31,7 @@ module.exports = {
     if (addedRoles.size > 0) {
       console.log(addedRoles.map(r => r.name).toString())
       if ((!addedRoles.map(r => r.name)) || (addedRoles.map(r => r.name).toString().length === 0)) return;
-      db.push(newMember.displayName, addedRoles.map(r => r.id));
+      db.push(newMember.user.id, addedRoles.map(r => r.id));
       console.log(`${oldMember.displayName} 現在是 ${addedRoles.map(r => r.name)} 了`);
       const newr1 = (`${oldMember.displayName} 現在是 ${addedRoles.map(r => r.name)} 了`);
       const Embed1 = (`身份組變了欸~\n\n${newr1}`)
