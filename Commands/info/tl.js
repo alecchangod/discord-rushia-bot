bbe4b5c3bd5b806d034e82bade476c17fb37e493
alecchangod <alec3d02@gmail.com>
2022-11-17 21:17:59 +0800

    added opition to choose for original language

--- Commands/info/tl.js
+++ Commands/info/tl.js
@@ -11,16 +11,18 @@ module.exports = {
     message.reply('loading').then(async (msg) => {
       var trantext = ""
       let text = message.content.split(" ");
-      var tarlang = text[2];
       var trantext = text[1];
+      var orilang = text[2];
+      var tarlang = text[3];
       if ((!tarlang) || (tarlang.length = 0)) var tarlang = await db.get(`${message.guild.id}_lang`);
         await wait(100);
-        // if ((!orilang) || (orilang.length = 0)) var orilang = "auto";
+        if ((!orilang) || (orilang.length = 0)) var orilang = "auto";
         if (tarlang === null) var tarlang = "zh-TW";
       if ((!trantext) || (trantext.length = 0)) return msg.delete(), message.reply('must provide text to translate!'), console.log(message, text, trantext);
-      if(JSON.stringify(lang).includes(tarlang) === false) return msg.edit("please enter a valid language code.");
-      const translation = await translate(trantext, tarlang);
-      msg.edit(`原語言：${translation.sourceLang} \n 譯文： ${translation.translation} \n ||沒錯就是Google渣翻||`)
+      if(JSON.stringify(lang).includes(tarlang) === false) return msg.edit("please enter a valid target language code.");
+      // if(JSON.stringify(lang).includes(orilang) === false) return msg.edit("please enter a valid language code.");
+      const translation = await translate(trantext, tarlang, orilang);
+      msg.edit(`語言：${translation.sourceLang} => ${translation.targetLang} \n 譯文： ${translation.translation} \n ||沒錯就是Google渣翻||`)
     }
     )
 
