fbecfa4c7daf2b2e9d829d60a8bad77a8e64a14f
alecchangod <alec3d02@gmail.com>
2022-11-17 21:16:59 +0800

    added message spliting and fixed fetching original message

--- Commands/info/msgupdate.js
+++ Commands/info/msgupdate.js
@@ -46,3 +46,51 @@ module.exports = {
     };
 }
 }
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SelectMenuBuilder } = require("discord.js");
+const { ApplicationCommandOptionType, ButtonStyle } = require("discord.js")
+module.exports = {
+    name: "msgupdate",
+    aliases: ["m"],
+    run: async (client, oldMessage, newMessage, secret) => {
+        // craete function
+      function split(str, channel) {
+        var partsArr = str.match(/[\s\S]{1,1900}/g) || [];
+        if (partsArr.length > 1) {
+          partsArr.forEach((partsArr, i) => channel.send(`${partsArr} \nPart ${i + 1} / ${partsArr.length}`));
+        }
+        else {
+          channel.send(str);
+        }
+      }
+      
+      // define message type and log them
+        if (!newMessage) return;
+        if (newMessage.channel.name.toLowerCase().includes("log")) return;
+        if (newMessage.content === oldMessage?.content) return;
+        if ((newMessage.embeds[0]) && (newMessage.embeds[0].description) && (oldMessage.embeds[0]) && (oldMessage.embeds[0].description) ) {
+            if (newMessage.embed = oldMessage?.embed) return;
+            const oreceivedEmbed = oldMessage.embeds[0];
+            const nreceivedEmbed = newMessage.embeds[0];
+            const exampleEmbed = new EmbedBuilder(oreceivedEmbed).setTitle('New title');
+            client.channels.fetch(secret.edit_log_channel).then(log => {
+                log.send({content: `頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} 原embed： `, embeds: [oreceivedEmbed]});
+                log.send({content: `頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} 新embed： `, embeds: [nreceivedEmbed]});
+            })
+            let channel = newMessage.guild.channels.cache.find(ch => ch.name.toLowerCase() === 'log');
+            if (!channel) return;
+            channel.send({content: `頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} 原embed： `, embeds: [oreceivedEmbed]});
+            channel.send({content: `頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} 新embed： `, embeds: [nreceivedEmbed]});
+          }
+        else{
+        if (!oldMessage) var oldMessage = 'not recorded'
+        client.channels.fetch(secret.edit_log_channel).then(log => {
+            var str = (`群組： ${newMessage.guild.name} \n  頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} \n 原信息： ${oldMessage} ,\n 新信息： ${newMessage}`);
+            split(str, log)
+        })
+        let channel = newMessage.guild.channels.cache.find(ch => ch.name.toLowerCase() === 'log');
+        if (!channel) return;
+        var str = (`頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} 原信息： ${oldMessage} ,\n 新信息： ${newMessage}`);
+        split(str, channel)
+    };
+}
+}
--- Commands/info/send-logger.js
+++ Commands/info/send-logger.js
@@ -1,6 +1,7 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SelectMenuBuilder } = require("@discordjs/builders")
 const { ApplicationCommandOptionType, ButtonStyle } = require("discord.js")
 module.exports = {
+<<<<<<< HEAD
     name: "send-logger", 
     aliases: ["s"],
     run: async (client, message, secret) => {
@@ -36,31 +37,82 @@ module.exports = {
               content: data
             });
           }); break;
+=======
+  name: "send-logger",
+  aliases: ["s"],
+  run: async (client, message, secret) => {
+    try {
+      // craete function
+      function split(str, channel) {
+        var partsArr = str.match(/[\s\S]{1,1900}/g) || [];
+        if (partsArr.length > 1) {
+          partsArr.forEach((partsArr, i) => channel.send(`${partsArr} \nPart ${i + 1} / ${partsArr.length}`));
+        }
+        else {
+          channel.send(str);
+>>>>>>> 61925c7 (added message spliting and fixed fetching original message)
         }
       }
-
-      else if ((message.embeds[0]) && (message.embeds[0].description) ) {
-      // check to ensure message was sent by bot and contains embed
       
-      const receivedEmbed = message.embeds[0];
-      const exampleEmbed = new EmbedBuilder(receivedEmbed).setTitle('New title');
-      client.channels.fetch(secret.log_channel).then(channel => {
-        channel.send({content: `人: ${message.author.tag} , 群: ${message.guild.name} , 頻道: ${message.channel.name} , embed:`, embeds: [receivedEmbed] })
-      }).catch(err => {
-        console.log(err)
-      })
-    }
+      // define message type and log them
+      if (message.channel.id === '994459707580358656') return;
+      if (message.channel.name.toLowerCase().includes("log")) return;
+      client.channels.fetch(secret.log_channel).then(async channel => {
+        if (message.stickers.size > 0) { console.log(message.stickers), channel.send('人:' + message.author.tag + ' , 訊息: 啊就貼圖(X , 群:' + message.guild.name + ' , 頻道:' + message.channel.name, { split: true }) }
+
+        else if (message.attachments.size > 0) {
+          var attachments = message.attachments;
+          for (let file of attachments) {
+            var s = 0;
+            message.attachments.forEach(a => {
+              s = s + a.size;
+
+            })
+            if (s > 10485760) {
+              if (message.content.length == 0) var data = `人: ${message.author.tag} , 群: ${message.guild.name} , 頻道: ${message.channel.name} , 附件: ${a.url}`
+              else if (message.content.length > 0) var data = `人: ${message.author.tag} , 訊息: ${message.content} , 群: ${message.guild.name} , 頻道: ${message.channel.name} , 附件: ${a.url}`
+              client.channels.fetch(secret.log_channel).then(channel1 => {
+                split(data, channel1)
+              });
+              break;
+            }
+            else client.channels.fetch(secret.log_channel).then(channel1 => {
+              if (message.content.length == 0) var data = `人: ${message.author.tag} , 群: ${message.guild.name} , 頻道: ${message.channel.name} , 附件:`
+              else if (message.content.length > 0) var data = `人: ${message.author.tag} , 訊息: ${message.content} , 群: ${message.guild.name} , 頻道: ${message.channel.name} , 附件:`
+              split(data, channel1)
+              channel1.send({
+                files: Array.from(message.attachments.values())
+              });
+            }); break;
+          }
+        }
+
+        else if ((message.embeds[0]) && (message.embeds[0].description)) {
+          // check to ensure message was sent by bot and contains embed
+          const receivedEmbed = message.embeds[0];
+          const exampleEmbed = new EmbedBuilder(receivedEmbed).setTitle('New title');
+          client.channels.fetch(secret.log_channel).then(channel => {
+            channel.send({ content: `人: ${message.author.tag} ,\n 群: ${message.guild.name} ,\n 頻道: ${message.channel.name} ,\n 内容(如有)： ${message.content},\n embed:`, embeds: [receivedEmbed] })
+          }).catch(err => {
+            console.log(err)
+          })
+        }
 
 
-    else if (message.reference){
-      const repliedTo = message.channel.messages.fetch(message.reference.messageId);
-      if(!repliedTo.author) return channel.send('人:' + message.author.tag + '訊息: ' + message.content + ' , 群:' + message.guild.name + ' , 頻道:' + message.channel.name, { split: true })
-      else channel.send('人:' + message.author.tag + ' , 前文(?: ' + repliedTo.author.tag + '\n ' + repliedTo.content + '訊息: ' + message.content + ' , 群:' + message.guild.name + ' , 頻道:' + message.channel.name, { split: true })
-  } 
+        else if (message.reference?.messageId) {
+          const repliedTo = await message.channel.messages.fetch(message.reference.messageId);
+            var str = `人:${message.author.tag} ,\n 前文(?: ${repliedTo.author.tag}\n 内容：${repliedTo.content} \n訊息: ${message.content} ,\n 群:${message.guild.name} ,\n 頻道:${message.channel.name}`
+            split(str, channel)
+        }
 
-    else { channel.send('人:' + message.author.tag + '訊息: ' + message.content + ' , 群:' + message.guild.name + ' , 頻道:' + message.channel.name, { split: true }) }
-  
-  }  
-  )} catch(e) {console.log(e)}
+        else {
+          var str = '人:' + message.author.tag + '訊息: ' + message.content + ' , 群:' + message.guild.name + ' , 頻道:' + message.channel.name
+          split(str, channel);
+        }
+
+      }
+      )
+    } catch (e) { console.log(e) }
+  }
 }
-}
+
