568829f4ae54ac3cf61b6f3cf167a2af21ef7c07
alecchangod <alec3d02@gmail.com>
2022-11-21 20:17:18 +0800

    moved to new dir.

---
+++ Commands/cmd/getprefix.js
@@ -0,0 +1,31 @@
+const { QuickDB } = require("quick.db");
+const db = new QuickDB({ filePath: "database/prefix.sqlite" }); 
+module.exports = {
+    name : 'getprefix', 
+    aliases : ['gp'], 
+    description : 'get prefix/serer', 
+    run : async(client, message, args, prefix) => {
+
+    // Getting an object from the database:
+    const a = await db.get(`prefix_${message.guild.id}`);
+
+    const aa = await db.get(`c_${message.guild.id}`);
+
+    const aaa = await db.get(`t_${message.guild.id}`);
+
+    var msg = `current prefix: ${a} \n set by ${aa} \n in <t:${aaa}>`;
+    if(a == null) {msg = `using default prefix now`}
+    message.reply(msg)
+
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ Commands/cmd/tl.js
@@ -0,0 +1,30 @@
+const { translate } = require("@almeidx/translate");
+const lang = require('../../lang.json')
+const wait = require('node:timers/promises').setTimeout;
+const { QuickDB } = require("quick.db");
+const db = new QuickDB({ filePath: "database/lang.sqlite" });
+
+module.exports = {
+  name: "tl",
+  aliases: ["t"],
+  run: async (client, message, args) => {
+    message.reply('loading').then(async (msg) => {
+      var trantext = ""
+      let text = message.content.split(" ");
+      var trantext = text[1];
+      var orilang = text[2];
+      var tarlang = text[3];
+      if ((!tarlang) || (tarlang.length = 0)) var tarlang = await db.get(`${message.guild.id}_lang`);
+        await wait(100);
+        if ((!orilang) || (orilang.length = 0)) var orilang = "auto";
+        if (tarlang === null) var tarlang = "zh-TW";
+      if ((!trantext) || (trantext.length = 0)) return msg.delete(), message.reply('must provide text to translate!'), console.log(message, text, trantext);
+      if(JSON.stringify(lang).includes(tarlang) === false) return msg.edit("please enter a valid target language code.");
+      // if(JSON.stringify(lang).includes(orilang) === false) return msg.edit("please enter a valid language code.");
+      const translation = await translate(trantext, tarlang, orilang);
+      msg.edit(`語言：${translation.sourceLang} => ${translation.targetLang} \n 譯文： ${translation.translation} \n ||沒錯就是Google渣翻||`)
+    }
+    )
+
+  }
+}
