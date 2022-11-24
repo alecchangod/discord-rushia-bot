e5c44f248d4aed5222219a9c2a6db71afeb93762
alecchangod <alec3d02@gmail.com>
2022-11-24 19:26:36 +0800

    improved logging

--- Commands/info/msgupdate.js
+++ Commands/info/msgupdate.js
@@ -19,15 +19,15 @@ module.exports = {
       // define message type and log them
         if (!newMessage) return;
         if (newMessage.channel.name.toLowerCase().includes("log")) return;
-        if (newMessage.content === oldMessage?.content) return;
-        if ((newMessage.embeds[0]) && (newMessage.embeds[0].description) && (oldMessage.embeds[0]) && (oldMessage.embeds[0].description) ) {
+        if ((newMessage.content === oldMessage?.content) && (newMessage.embed === oldMessage?.embeds)) return;
+        if ((newMessage.embeds[0]?.description)) { //  && (newMessage.embeds[0]) && (oldMessage.embeds[0]) && (oldMessage.embeds[0].description) 
             if (newMessage.embed = oldMessage?.embed) return;
             const oreceivedEmbed = oldMessage.embeds[0];
             const nreceivedEmbed = newMessage.embeds[0];
             const exampleEmbed = new EmbedBuilder(oreceivedEmbed).setTitle('New title');
             client.channels.fetch(secret.edit_log_channel).then(log => {
-                log.send({content: `頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} 原embed： `, embeds: [oreceivedEmbed]});
-                log.send({content: `頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} 新embed： `, embeds: [nreceivedEmbed]});
+                log.send({content: `頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} \n 原信息(如有)： ${oldMessage?.content}\n 原embed： `, embeds: [oreceivedEmbed]});
+                log.send({content: `頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} \n 新信息(如有)： ${newMessage.content}\n 新embed： `, embeds: [nreceivedEmbed]});
             })
             let channel = newMessage.guild.channels.cache.find(ch => ch.name.toLowerCase() === 'log');
             if (!channel) return;
@@ -37,12 +37,12 @@ module.exports = {
         else{
         if (!oldMessage) var oldMessage = 'not recorded'
         client.channels.fetch(secret.edit_log_channel).then(log => {
-            var str = (`群組： ${newMessage.guild.name} \n  頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} \n 原信息： ${oldMessage} ,\n 新信息： ${newMessage}`);
+            var str = (`群組： ${newMessage.guild.name} \n  頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} \n 原信息： ${oldMessage} \n ======================================== \n 新信息： ${newMessage}`);
             split(str, log)
         })
         let channel = newMessage.guild.channels.cache.find(ch => ch.name.toLowerCase() === 'log');
         if (!channel) return;
-        var str = (`頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} 原信息： ${oldMessage} ,\n 新信息： ${newMessage}`);
+        var str = (`頻道： ${newMessage.channel} \n 人：${newMessage.author.tag} 原信息： ${oldMessage} \n ======================================== \n 新信息： ${newMessage}`);
         split(str, channel)
     };
 }
