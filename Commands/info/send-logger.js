1b04ff142e8856ab09e2f9457b30b7d5a7d85106
alecchangod <alec3d02@gmail.com>
2022-12-26 17:38:41 +0800

    fix conflict when merging repo

--- Commands/info/send-logger.js
+++ Commands/info/send-logger.js
@@ -1,43 +1,6 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SelectMenuBuilder } = require("@discordjs/builders")
 const { ApplicationCommandOptionType, ButtonStyle } = require("discord.js")
 module.exports = {
-<<<<<<< HEAD
-    name: "send-logger", 
-    aliases: ["s"],
-    run: async (client, message, secret) => {
-        try{
-    if (message.channel.id === '994459707580358656') return;
-    if (message.channel.name.toLowerCase().includes("log")) return;
-    var channel = client.channels.fetch(secret.log_channel).then(channel => {
-      if(message.stickers.size > 0) {channel.send('人:' + message.author.tag + ' , 訊息: 啊就貼圖(X , 群:' + message.guild.name + ' , 頻道:' + message.channel.name, { split: true })}
-
-      else if (message.attachments.size > 0) {
-        var attachments = message.attachments;
-        for (let file of attachments) {
-          var s = 0;
-          message.attachments.forEach(a=>{
-            s=s+a.size;
-          
-          });
-             if (s > 10485760) {
-                    if (message.content.length == 0) var data = `人: ${message.author.tag} , 群: ${message.guild.name} , 頻道: ${message.channel.name} , 附件: ${a.url}`
-                    else if (message.content.length > 0) var data = `人: ${message.author.tag} , 訊息: ${message.content} , 群: ${message.guild.name} , 頻道: ${message.channel.name} , 附件: ${a.url}`
-            client.channels.fetch(secret.log_channel).then(channel1 => {
-            channel1.send({
-              content: data
-            });
-          });
-          break;
-          }
-          else client.channels.fetch(secret.log_channel).then(channel1 => {
-            if (message.content.length == 0) var data = `人: ${message.author.tag} , 群: ${message.guild.name} , 頻道: ${message.channel.name} , 附件:`
-            else if (message.content.length > 0) var data = `人: ${message.author.tag} , 訊息: ${message.content} , 群: ${message.guild.name} , 頻道: ${message.channel.name} , 附件:`
-            channel1.send({
-              files: Array.from(message.attachments.values()),
-              content: data
-            });
-          }); break;
-=======
   name: "send-logger",
   aliases: ["s"],
   description: 'log message sent(owner only for now)',
