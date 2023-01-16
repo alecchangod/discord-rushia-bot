02c5dcda9aede9aff3902be7692e2a260010b23b
Alec Chan <alec3d02@gmail.com>
2023-01-16 19:55:02 +0800

    slash: time: import translation from chatbox command

--- SlashCommands/Info/time.js
+++ SlashCommands/Info/time.js
@@ -2,8 +2,11 @@ module.exports = {
   name: "time",
   description: "get current time",
   run: async (client, interaction, args) => {
+    var langc = "zh-TW";
+    console.log(trans.filter(it => it.name === "time")[0].lang.filter(it => it.code === langc)[0].trans) //.filter(it => it.lang === "en")[0]
+    var tt = trans.filter(it => it.name === "time")[0].lang.filter(it => it.code === langc)[0].trans
 
-    interaction.reply(`<t:${Math.floor(new Date() / 1000)}>`)
+    interaction.reply(`${tt}<t:${Math.floor(new Date() / 1000)}>`)
     console.log(interaction.guild.preferredLocale)
 
       // var a = client.channels.cache.get(message.channel.id)
