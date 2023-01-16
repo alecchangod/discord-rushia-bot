20d373ddb6dc704c9c577aec2d6b00beb9e19cde
Alec Chan <alec3d02@gmail.com>
2023-01-16 16:07:22 +0800

    cmd: join: remove addition spacing that cause issue running

--- Commands/info/join.js
+++ Commands/info/join.js
@@ -1,37 +1,36 @@
 module.exports = {
-    name: "join", 
-    aliases: ["j"],
-    description : 'send a message when user join(hardcoded for now)', 
-    run: async(client, message, secret, member) => {
-        try {                  
-            if (member.guild.id == secret.grp) {
-                const channelId = secret.channelID
-                try {
-                      (member.guild.channels.fetch(channelId)).then((channel => {
-                        if (channel.guild == secret.grp) {
-                          channel.send('欸有新人~')
-                        }
-                      }))
-                    } catch (e) { console.log(e) };
-                    if (!member.message.author.bot) {
-                      member.send('早安')
-                    }
-                  }
+  name: "join",
+  aliases: ["j"],
+  description: 'send a message when user join(hardcoded for now)',
+  run: async (client, secret, member) => {
+    try {
+      if (member.guild.id == secret.grp) {
+        const channelId = secret.channelID
+        try {
+          (member.guild.channels.fetch(channelId)).then((channel => {
+            if (channel.guild == secret.grp) {
+              channel.send('欸有新人~')
+            }
+          }))
+        } catch (e) { console.log(e) };
+        if (!member.message.author.bot) {
+          member.send('早安')
+        }
+      }
 
+      if (member.guild.id == secret.grp1) {
+        const channelId1 = secret.channelID1
+        try {
+          (member.guild.channels.fetch(channelId1)).then((channel1 => {
+            if (channel1.guild == secret.grp1) {
+              channel1.send('欸有新人~')
+            }
+          }))
+        } catch (e) { console.log(e) }
+      }
 
-            if (member.guild.id == secret.grp1) {
-                    const channelId1 = secret.channelID1 
-                    try {
-                      (member.guild.channels.fetch(channelId1)).then((channel1 => {
-                        if (channel1.guild == secret.grp1) {
-                          channel1.send('欸有新人~')
-                        }
-                      }))
-                    } catch (e) { console.log(e) }
-                  }
-                
-          } catch (e) {
-            console.log(e)
-          }
+    } catch (e) {
+      console.log(e)
     }
   }
+}
--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -3,9 +3,9 @@ const client = require('../../index.js')
 const secret = require('../../config.json')
 
 //new member
-client.on('guildMemberAdd', async member => {
-    const cmd = 'join';
-    let command = client.info.get(cmd)
-    if (!command) command = client.info.get(client.aliases.get(cmd));
-    if (command) command.run(client, secret, member)
-  });
+client.on('guildMemberAdd', async (member) => {
+  const cmd = 'join';
+  let command = client.info.get(cmd)
+  if (!command) command = client.info.get(client.aliases.get(cmd));
+  if (command) command.run(client, secret, member)
+});
