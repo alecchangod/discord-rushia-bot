b2122026f07f0175af50b92a0cd860995e0382c2
Alec Chan <alec3d02@gmail.com>
2023-01-16 19:56:33 +0800

    clean up the file

--- SlashCommands/Info/time.js
+++ SlashCommands/Info/time.js
@@ -3,14 +3,7 @@ module.exports = {
   description: "get current time",
   run: async (client, interaction, args) => {
     var langc = "zh-TW";
-    console.log(trans.filter(it => it.name === "time")[0].lang.filter(it => it.code === langc)[0].trans) //.filter(it => it.lang === "en")[0]
-    var tt = trans.filter(it => it.name === "time")[0].lang.filter(it => it.code === langc)[0].trans
-
+    var tt = trans.filter(it => it.name === "time")[0].lang.filter(it => it.code === langc)[0].trans;
     interaction.reply(`${tt}<t:${Math.floor(new Date() / 1000)}>`)
-    console.log(interaction.guild.preferredLocale)
-
-      // var a = client.channels.cache.get(message.channel.id)
-      // console.log(a)
-      // if (message.guile.me.permissionsIn(a).has("SEND_MESSAGES")) return message.reply('send message permission')
   }
 }
