d2128738cf640b35ef6483d87708af9f7f848bce
Alec Chan <alec3d02@gmail.com>
2023-02-19 20:35:20 +0800

    slash: say: add opition to reply a message

--- SlashCommands/Info/say.js
+++ SlashCommands/Info/say.js
@@ -3,26 +3,42 @@ const { ApplicationCommandOptionType, ButtonStyle } = require("discord.js")
 const { PermissionFlagsBits } = require('discord-api-types/v10');
 
 module.exports = {
-    name: "say", 
-    description: "send message as bot",
-    options: [
-      {
-          name: 'content',
-          description: 'enter message that you want to send',
-          type: ApplicationCommandOptionType.String,
-          require: true
-      }
+  name: "say",
+  description: "send message as bot",
+  options: [
+    {
+      name: 'content',
+      description: 'enter message that you want to send',
+      type: ApplicationCommandOptionType.String,
+      require: true
+    },
+    {
+      name: 'id',
+      description: 'enter message id that you want to reply',
+      type: ApplicationCommandOptionType.String,
+      require: false
+    }
   ],
   userPermissions: PermissionFlagsBits.ManageMessages,
-    run: async(client, interaction, args, secret) => {
-        try {
-          const content = interaction.options.getString('content');
-          interaction.reply({ content: `${content} are being sent to <#${interaction.channelId}>`, ephemeral: true });
-          client.channels.fetch(interaction.channelId).then(async chid => {
-              chid.send(content)
-          })
-          } catch (e) {
-            console.log(e)
-          }
+  run: async (client, interaction, args, secret) => {
+    try {
+      const content = interaction.options.getString('content');
+      const id = interaction.options.getString('id');
+      client.channels.fetch(interaction.channelId).then(async chid => {
+        if (id) {
+          const msg = await chid.messages.fetch(id);
+          console.log(msg);
+          if (!msg) return
+          msg.reply(content);
+          interaction.reply({ content: `${content} has been sent to <#${interaction.channelId}>`, ephemeral: true });
+        }
+        else {
+          chid.send(content);
+          interaction.reply({ content: `${content} has been sent to <#${interaction.channelId}>`, ephemeral: true });
+        }
+      })
+    } catch (e) {
+      console.log(e)
     }
   }
+}
