0a31365f146fb318c55eaf867c804f1cd2f2b5f8
Alec Chan <alec3d02@gmail.com>
2023-03-17 17:59:04 +0800

    ready: disable twitter streaming for now
    node:events:491
          throw er; // Unhandled 'error' event
          ^
    
    Error: Bad Twitter streaming request: 403
        at Object.exports.makeTwitError (C:\Users\A\OneDrive\Desktop\git\rushia\node_modules\twit\lib\helpers.js:74:13)
        at Request.<anonymous> (C:\Users\A\OneDrive\Desktop\git\rushia\node_modules\twit\lib\streaming-api-connection.js:96:29)
        at Request.emit (node:events:525:35)
        at Gunzip.<anonymous> (C:\Users\A\OneDrive\Desktop\git\rushia\node_modules\request\request.js:1076:12)
        at Object.onceWrapper (node:events:627:28)
        at Gunzip.emit (node:events:513:28)
        at endReadableNT (node:internal/streams/readable:1358:12)
        at processTicksAndRejections (node:internal/process/task_queues:83:21)
    Emitted 'error' event on StreamingAPIConnection instance at:
        at Request.<anonymous> (C:\Users\A\OneDrive\Desktop\git\rushia\node_modules\twit\lib\streaming-api-connection.js:99:14)
        at Request.emit (node:events:525:35)
        [... lines matching original stack trace ...]
        at processTicksAndRejections (node:internal/process/task_queues:83:21) {
      code: null,
      allErrors: [],
      twitterReply: '<html>\\n<head>\\n<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>\\n<title>Error 403 \n' +
        'Please use V2 filtered and sample volume stream as alternatives\n' +
        '</title>\n' +
        '</head>\n' +
        '<body>\n' +
        '<h2>HTTP ERROR: 403</h2>\n' +
        "<p>Problem accessing '/1.1/statuses/filter.json'. Reason:\n" +
        '<pre>    \n' +
        'Please use V2 filtered and sample volume stream as alternatives\n' +
        '</pre>\n' +
        '</body>\n' +
        '</html>\n',
      statusCode: 403
    }

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,61 +2,61 @@
 const client = require('../../index.js')
 const secret = require('../../config.json')
 const cron = require('cron');
-//start-up activities
+// start-up activities
 client.on('ready', async () => {
 
-    console.log(`${client.user.tag} is ready on ${client.guilds.cache.size} servers.`);
-    client.guilds.cache.forEach(guild => console.log(`${guild.name}(${guild.id}), ${guild.memberCount} user, ${guild.roles.cache.size} roles, ${guild.channels.cache.size} channels`))
-    // console.log(client.guilds.cache)
-    // msgCount = await client.guilds.cache.message.size;
-    // console.log(`${msgCount}`);
-  
-    const { QuickDB } = require("quick.db");
-    const db = new QuickDB({ filePath: "database/group.sqlite" });
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.guilds.cache.forEach(guild => (async () => { await db.set(guild.name, guild.id) })());
-    client.user.setPresence({ activities: [{ name: "誰在做夢", type: 3 }], status: 'idle', clientStatus: "PS5" }); 
-    //send scheduled message
-    let scheduledMessage = new cron.CronJob('00 00 12 * * *', () => {
-      const guild = client.guilds.cache.get(secret.grp2);
-      const channel = guild.channels.cache.get(secret.channelID2);
-      channel.send('你各位別當死魚堆');
-    });
-    scheduledMessage.start()
-  });
+  console.log(`${client.user.tag} is ready on ${client.guilds.cache.size} servers.`);
+  client.guilds.cache.forEach(guild => console.log(`${guild.name}(${guild.id}), ${guild.memberCount} user, ${guild.roles.cache.size} roles, ${guild.channels.cache.size} channels`))
+  // console.log(client.guilds.cache)
+  // msgCount = await client.guilds.cache.message.size;
+  // console.log(`${msgCount}`);
 
-  
-//self role
+  const { QuickDB } = require("quick.db");
+  const db = new QuickDB({ filePath: "database/group.sqlite" });
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.guilds.cache.forEach(guild => (async () => { await db.set(guild.name, guild.id) })());
+  client.user.setPresence({ activities: [{ name: "誰在做夢", type: 3 }], status: 'idle', clientStatus: "PS5" });
+  //send scheduled message
+  let scheduledMessage = new cron.CronJob('00 00 12 * * *', () => {
+    const guild = client.guilds.cache.get(secret.grp2);
+    const channel = guild.channels.cache.get(secret.channelID2);
+    channel.send('你各位別當死魚堆');
+  });
+  scheduledMessage.start()
+});
+
+
+// self role
 client.once('ready', () => {
-    let channel = client.channels.fetch('963802334482284595').then(channel => {
-      try {
-        channel.messages.fetch('963802394045583370').then(message => {
-          message.react('<:makaneko_surprise:958407417559908382>')
-          const filter = (reaction, user) => {
-            return reaction.emoji.name === 'makaneko_surprise';
-          };
-  
-  
-          const collector = message.createReactionCollector({ filter });
-  
-  
-          collector.on('collect', (reaction, user) => {
-            const role = message.guild.roles.fetch('964140235401355304');
-  
-            message.guild.members.fetch(user.id).then(member => {
-              member.roles.add('964140235401355304');
-            });
+  let channel = client.channels.fetch('963802334482284595').then(channel => {
+    try {
+      channel.messages.fetch('963802394045583370').then(message => {
+        message.react('<:makaneko_surprise:958407417559908382>')
+        const filter = (reaction, user) => {
+          return reaction.emoji.name === 'makaneko_surprise';
+        };
+
+
+        const collector = message.createReactionCollector({ filter });
+
+
+        collector.on('collect', (reaction, user) => {
+          const role = message.guild.roles.fetch('964140235401355304');
+
+          message.guild.members.fetch(user.id).then(member => {
+            member.roles.add('964140235401355304');
           });
-        })
-      } catch (e) { console.log(e) }
-    })
-  });
+        });
+      })
+    } catch (e) { console.log(e) }
+  })
+});
 
-  
-// //twitter track start
-client.once('ready', () => {
-    const cmd = 'twitter-main';
-    let command = client.twitter.get(cmd)
-    if (!command) command = client.twitter.get(client.aliases.get(cmd));
-    if (command) command.run(client, secret)
-  });
+
+// // twitter track start
+// client.once('ready', () => {
+//   const cmd = 'twitter-main';
+//   let command = client.twitter.get(cmd)
+//   if (!command) command = client.twitter.get(client.aliases.get(cmd));
+//   if (command) command.run(client, secret)
+// });
