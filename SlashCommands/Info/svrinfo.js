275a16e7f0d88227360b214245cfe01a13be4810
Alec Chan <alec3d02@gmail.com>
2023-03-23 22:47:50 +0800

    slash: svrinfo: message --> interaction

--- SlashCommands/Info/svrinfo.js
+++ SlashCommands/Info/svrinfo.js
@@ -7,15 +7,15 @@ module.exports = {
     run: async (client, interaction, args, secret, trans) => {
 
         // Getting prefix from the database
-        var pre = await db.get(`prefix_${message.guild.id}`),
-            author = await db.get(`c_${message.guild.id}`),
-            time = await db.get(`t_${message.guild.id}`);
+        var pre = await db.get(`prefix_${interaction.guild.id}`),
+            author = await db.get(`c_${interaction.guild.id}`),
+            time = await db.get(`t_${interaction.guild.id}`);
 
         // Getting group language from the database
         const lang = await db.get(`${interaction.guild.id}_lang`);
 
         if (pre == null) { pre = `=(Default)` }
-        var msg = `**Info for ${message.guild.name}** \n \n Group Language Code: \`\`${lang}\`\` \n \n Current prefix: \`\`${pre}\`\` \n Set by \`\`${author}\`\` \n At <t:${time}>`;
+        var msg = `**Info for ${interaction.guild.name}** \n \n Group Language Code: \`\`${lang}\`\` \n \n Current prefix: \`\`${pre}\`\` \n Set by \`\`${author}\`\` \n At <t:${time}>`;
         interaction.reply(msg)
     }
 }
