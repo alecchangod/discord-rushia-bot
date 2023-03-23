ddc73655429d1b45bac6f129b7e68fb5e83ecdc3
Alec Chan <alec3d02@gmail.com>
2023-03-24 00:32:33 +0800

    cmd: time: new way to find translate

--- Commands/cmd/time.js
+++ Commands/cmd/time.js
@@ -3,8 +3,9 @@ module.exports = {
   aliases: ["time"],
   description: 'get current time',
   run: async (client, message, args, secret, prefix, trans, langc) => {
-    var langc = "zh-TW";
-    var tt = trans.filter(it => it.name === "time")[0].lang.filter(it => it.code === langc)[0].trans;
-    message.reply(`${tt}<t:${Math.floor(new Date() / 1000)}>`)
+    // var langc = "zh-TW";
+    console.log(langc);
+    var current_time = trans.filter(it => it.name === "time")[0].lang.filter(it => it.code === langc)[0].strings.filter(it => it.name === "current_time")[0].trans;
+    message.reply(`${current_time}<t:${Math.floor(new Date() / 1000)}>`)
   }
 }
