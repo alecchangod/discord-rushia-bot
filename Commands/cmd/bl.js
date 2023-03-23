a4e2ac67b37ae0ebc21d717cf60ef84bc86b85ef
Alec Chan <alec3d02@gmail.com>
2023-03-24 00:33:06 +0800

    cmd: bl new translation

--- Commands/cmd/bl.js
+++ Commands/cmd/bl.js
@@ -7,7 +7,8 @@ module.exports = {
     description: 'Block words in a group(case sensitive)',
     run: async (client, message, args, secret, prefix, trans, langc) => {
         var user = await message.guild.members.fetch(message.author)
-        if (!user.permissions.has(PermissionsBitField.Flags.ManageMessages)) return message.channel.send("笑死你沒權限")
+        var missing_permission = trans.filter(it => it.name === "bl")[0].lang.filter(it => it.code === langc)[0].strings.filter(it => it.name === "missing_permission")[0].trans;
+        if (!user.permissions.has(PermissionsBitField.Flags.ManageMessages)) return message.channel.send(missing_permission) //""
         var det = message.content.toLowerCase().split(" "),
             status = det[1],
             word = det[2];
@@ -15,19 +16,24 @@ module.exports = {
             (async () => {
                 await db.push('group', message.guild.id);
                 var now = await db.get(message.guild.id);
-                if (JSON.stringify(now).includes(word)) return message.reply("Word already banned.");
+                var already_banned = trans.filter(it => it.name === "bl")[0].lang.filter(it => it.code === langc)[0].strings.filter(it => it.name === "already_banned")[0].trans;
+                if (JSON.stringify(now).includes(word)) return message.reply(already_banned);//""
                 await db.push(`${message.guild.id}`, word);
-                message.reply(`\`\`\`${word}\`\`\` was now banned. Try to send it <a:isis:963826754328330300>`);
+                var banned = trans.filter(it => it.name === "bl")[0].lang.filter(it => it.code === langc)[0].strings.filter(it => it.name === "banned")[0].trans;
+                message.reply(`\`\`\`${word}\`\`\` ${banned} <a:isis:963826754328330300>`);
             })();
         }
         if (status === "del") {
             (async () => {
                 var grp = await db.get('group');
-                if (JSON.stringify(grp).includes(message.guild.id) === false) return message.reply("No words were banned in this server yet!");
+                var no_word_banned = trans.filter(it => it.name === "bl")[0].lang.filter(it => it.code === langc)[0].strings.filter(it => it.name === "no_word_banned")[0].trans;
+                if (JSON.stringify(grp).includes(message.guild.id) === false) return message.reply(no_word_banned);
                 var now = await db.get(message.guild.id);
-                if (JSON.stringify(now).includes(word) === false) return message.reply("Word not yet banned.");
+                var not_banned = trans.filter(it => it.name === "bl")[0].lang.filter(it => it.code === langc)[0].strings.filter(it => it.name === "not_banned")[0].trans;
+                if (JSON.stringify(now).includes(word) === false) return message.reply(not_banned);
                 await db.pull(`${message.guild.id}`, word);
-                message.reply(`\`\`\`${word}\`\`\` was now unbanned.`)
+                var unbanned = trans.filter(it => it.name === "bl")[0].lang.filter(it => it.code === langc)[0].strings.filter(it => it.name === "unbanned")[0].trans;
+                message.reply(`\`\`\`${word}\`\`\` ${unbanned}`)
             })();
         }
     }
