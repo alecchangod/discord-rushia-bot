6bb2c31c3d1f338ae1afee77908d3f69e28e935d
Alec Chan <alec3d02@gmail.com>
2023-04-22 02:00:03 +0800

    slash: tl: increase wait from 100 to 500 and clean up some comment
    [ERROR] Unhandled promise rejection: The reply to this interaction has not been sent or deferred..
    Error [InteractionNotReplied]: The reply to this interaction has not been sent or deferred.
        at ChatInputCommandInteraction.editReply (C:\Users\A\OneDrive\Desktop\git\rushia\node_modules\discord.js\src\structures\interfaces\InteractionResponses.js:157:48)
        at Object.run (C:\Users\A\OneDrive\Desktop\git\rushia\SlashCommands\Info\tl.js:57:29)
        at processTicksAndRejections (node:internal/process/task_queues:96:5) {
      code: 'InteractionNotReplied'
    }

--- SlashCommands/Info/tl.js
+++ SlashCommands/Info/tl.js
@@ -1,11 +1,10 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SelectMenuBuilder } = require("@discordjs/builders")
-const { ApplicationCommandOptionType, ButtonStyle } = require("discord.js")
-const wait = require('node:timers/promises').setTimeout;
-const { QuickDB } = require("quick.db");
-const db = new QuickDB({ filePath: "database/lang.sqlite" });
-const { translate } = require("@almeidx/translate");
-const lang = require('../../lang.json')
-
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SelectMenuBuilder } = require("@discordjs/builders"),
+      { ApplicationCommandOptionType, ButtonStyle } = require("discord.js"),
+      wait = require('node:timers/promises').setTimeout,
+      { QuickDB } = require("quick.db"),
+      db = new QuickDB({ filePath: "database/lang.sqlite" }),
+      { translate } = require("@almeidx/translate"),
+      lang = require('../../lang.json');
 
 module.exports = {
     name: 'tl',
@@ -35,22 +34,17 @@ module.exports = {
             content: "loading"
         })
 
-
-        var trantext = ""
-        var trantext = interaction.options.getString('text');
-        var tarlang = interaction.options.getString('tar-lang');
-        var orilang = interaction.options.getString('original-lang');
+        var trantext = "",
+            trantext = interaction.options.getString('text'),
+            tarlang = interaction.options.getString('tar-lang'),
+            orilang = interaction.options.getString('original-lang');
         if ((!tarlang) || (tarlang.length = 0)) var tarlang = await db.get(`${interaction.guild.id}_lang`);
-        await wait(100);
+        await wait(500);
         if ((!orilang) || (orilang.length = 0)) var orilang = "auto";
         if (tarlang === null) var tarlang = "zh-TW";
-        if(JSON.stringify(lang).includes(tarlang) === false) return await wait(2000), interaction.editReply("please input a valid language code.");
-        // var ori = translate.languages[orilang];
-        // console.log(orilang, tarlang, tar, ori);
-        // if (tar === undefined) return await wait(2000), interaction.editReply("please input a valid language code.");
-        // if (ori === undefined) return await wait(2000), interaction.editReply("please input a valid language code.");
+        if (JSON.stringify(lang).includes(tarlang) === false) return await wait(2000), interaction.editReply("please input a valid language code.");
         if ((!trantext) || (trantext.length = 0)) return await wait(2000), interaction.editReply('must provide text to translate!');
         const translation = await translate(trantext, tarlang, orilang);
-        interaction.editReply(`語言：${translation.sourceLang} ==> ${translation.targetLang} \n 譯文： ${translation.translation} \n ||沒錯就是Google渣翻||`)
+        await interaction.editReply(`語言：${translation.sourceLang} ==> ${translation.targetLang} \n 譯文： ${translation.translation} \n ||沒錯就是Google渣翻||`);
     }
 }
