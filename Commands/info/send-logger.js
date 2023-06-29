292689ab0c3149cfd79d8f2c89e0a1b26646b280
Alec Chan <alec3d02@gmail.com>
2023-06-30 01:37:05 +0800

    rushia: cmd: base: send-logger: full rewrite

--- Commands/info/send-logger.js
+++ Commands/info/send-logger.js
@@ -1,52 +1,35 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SelectMenuBuilder } = require("@discordjs/builders")
-const { ApplicationCommandOptionType, ButtonStyle } = require("discord.js")
+const { EmbedBuilder } = require("@discordjs/builders")
+
 module.exports = {
   name: "send-logger",
   aliases: ["s"],
   description: 'log message sent(owner only for now)',
-  run: async (client, message, secret, trans, langc) => {
+  run: async (client, message, secret) => {
     try {
-      // define message type and log them
-      if (message.channel.id === '994459707580358656') return;
-      if (message.channel.name.toLowerCase().includes("log")) return;
-      client.channels.fetch(secret.log_channel).then(async channel => {
-        await msgtype(message, channel);
-      }
-      )
-    } catch (e) { console.log(e) }
+      if (message.channel.id === '994459707580358656' || message.channel.name.toLowerCase().includes("log")) return;
+      
+      const channel = await client.channels.fetch(secret.log_channel);
+      await msgtype(message, channel);
+    } catch (e) { 
+      console.log(e) 
+    }
   }
-}
-
-// craete function
-// split message > 2000
+};
 function split(str, channel, file) {
   var partsArr = str.match(/[\s\S]{1,1900}/g) || [];
-  if (partsArr.length > 1) {
-    partsArr.forEach((partsArr, i) => {
-      if (!file) { channel.send(`${partsArr} \nPart ${i + 1} / ${partsArr.length}`) }
-      else { channel.send({ content: `${partsArr} \nPart ${i + 1} / ${partsArr.length}`, files: [file] }) }
-    });
-  }
-  else {
-    if (!file) channel.send(str)
-    else channel.send({ content: str, files: file })
-  }
-}
-// for embed
+  partsArr.forEach((part, i) => {
+    const content = `${part} \nPart ${i + 1} / ${partsArr.length}`;
+    channel.send(file ? { content, files: [file] } : content);
+  });
+};
 function embed(str, channel, embed) {
   const exampleEmbed = new EmbedBuilder(embed).setTitle('New title');
   var partsArr = str.match(/[\s\S]{1,1900}/g) || [];
-  if (partsArr.length > 1) {
-    partsArr.forEach((partsArr, i) => {
-      channel.send({ content: `${partsArr} \nPart ${i + 1} / ${partsArr.length}`, embeds: [embed] }) // if (!file) { }
-      // else { channel.send({ content: `${partsArr} \nPart ${i + 1} / ${partsArr.length}`, files: [file], embeds: embed }) }
-    });
-  }
-  else {
-    // if ((!file) || (file === undefined)) channel.send({content: str, embeds: embed})
-    channel.send({ content: str, embeds: [embed] })
-  }
-}
+  partsArr.forEach((part, i) => {
+    const content = `${part} \nPart ${i + 1} / ${partsArr.length}`;
+    channel.send({ content, embeds: [embed] });
+  });
+};
 // msgtype
 // 0 = stickers
 // 1 = attatchments url
@@ -54,81 +37,67 @@ function embed(str, channel, embed) {
 // 3 = embed + content
 // 4 = embed
 // 5 = reply
-var tries = 0 // prevent keep fetching message
+let tries = 0 // prevent keep fetching message
 // 6 = normal message
 async function msgtype(message, channel) {
   // if it was a sticker
   if (message.stickers.size > 0) {
-    var ext = "png", sck = message.stickers.first();
-    var sticurl = `https://cdn.discordapp.com/stickers/${sck.id}.${ext}`;
+    const ext = "png";
+    const sck = message.stickers.first();
+    const sticurl = `https://cdn.discordapp.com/stickers/${sck.id}.${ext}`;
+    let str = '';
+    let files = [];
     if (message.attachments.size > 0) {
-      message.attachments.forEach(attachments => {
-        if (attachments.size > 10485760) {
-          if (message.content.length == 0) var str = `人: ${message.author.tag} ,\n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name} ,\n 附件: ${attachments.url}`
-          else if (message.content.length > 0) var str = `人: ${message.author.tag} ,\n 訊息: ${message.content} ,\n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name} ,\n 附件: ${attachments.url}`
+      message.attachments.forEach(attachment => {
+        const hasContent = message.content.length > 0;
+        const attachmentText = attachment.size > 10485760 ? `, \n 附件: ${attachment.url}` : ', \n 附件:';
+        str = `人: ${message.author.tag} ,\n${hasContent ? ' 訊息: ' + message.content + ' ,' : ''} \n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name}${attachmentText}`;
+        if (attachment.size <= 10485760) {
+          files = Array.from(message.attachments.values());
         }
-        else {
-          if (message.content.length == 0) var data = `人: ${message.author.tag} ,\n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name} ,\n 附件:`
-          else if (message.content.length > 0) var data = `人: ${message.author.tag} ,\n 訊息: ${message.content} ,\n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name} ,\n 附件:`
-          var files = Array.from(message.attachments.values());
-        }
-      })
-      split(str, channel, files)
+      });
+      split(str, channel, files);
     }
     else {
-      if (message.content.length == 0)
-        var str = `人: ${message.author.tag} ,\n 訊息: ${message.content} , 貼圖： ${sticurl} , \n 群: ${message.guild.name} ,\n 頻道: ${message.channel.name}`;
-      else (message.content.length > 0)
-      var str = `人: ${message.author.tag} ,\n 訊息: ${message.content} , 貼圖： ${sticurl} , \n 群: ${message.guild.name} ,\n 頻道: ${message.channel.name}`;
-
-      split(str, channel)
+      const hasContent = message.content.length > 0;
+      str = `人: ${message.author.tag} ,\n${hasContent ? ' 訊息: ' + message.content + ' ,' : ''} \n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name}`; 
+      split(str, channel);
     }
-    var type = 0
+    let type = 0;
   }
-
   // if it has attachments (image/video/document....)
   else if (message.attachments.size > 0) {
-    message.attachments.forEach(attachments => {
-      if (attachments.size > 10485760) {
-        if (message.content.length == 0) var str = `人: ${message.author.tag} ,\n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name} ,\n 附件: ${attachments.url}`
-        else if (message.content.length > 0) var str = `人: ${message.author.tag} ,\n 訊息: ${message.content} ,\n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name} ,\n 附件: ${attachments.url}`
-        split(str, channel)
+    message.attachments.forEach(attachment => {
+      const hasContent = message.content.length > 0;
+      const attachmentText = attachment.size > 10485760 ? `, \n 附件: ${attachment.url}` : ', \n 附件:';
+      const str = `人: ${message.author.tag} ,\n${hasContent ? ' 訊息: ' + message.content + ' ,' : ''} \n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name}${attachmentText}`;
+      if (attachment.size <= 10485760) {
+        const files = Array.from(message.attachments.values());
+        split(str, channel, files);
+      } else {
+        split(str, channel);
       }
-      else {
-        if (message.content.length == 0) var str = `人: ${message.author.tag} ,\n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name} ,\n 附件:`
-        else if (message.content.length > 0) var str = `人: ${message.author.tag} ,\n 訊息: ${message.content} ,\n 群: ${message.guild.name} , 貼圖： ${sticurl} ,\n 頻道: ${message.channel.name} ,\n 附件:`
-        var files = Array.from(message.attachments.values());
-        split(str, channel, files)
-      }
-    })
+    });
   }
-
   // if it have embed
-  else if ((message.embeds[0])) {
-    // check to ensure message was sent by bot and contains embed
+  else if (message.embeds[0]) {
     const receivedEmbed = message.embeds[0];
-    if (message.content.length > 0) var str = `人: ${message.author.tag} ,\n 群: ${message.guild.name} ,\n 頻道: ${message.channel.name} ,\n 内容： ${message.content} \n embed:`, type = 3
-    else if (message.content.length == 0) var str = `人: ${message.author.tag} ,\n 群: ${message.guild.name} ,\n 頻道: ${message.channel.name} ,\n embed:`, type = 4
-    var embeds = receivedEmbed
-    embed(str, channel, embeds)
-  }
-
-
+    const hasContent = message.content.length > 0;
+    let type = hasContent ? 3 : 4;
+    const str = `人: ${message.author.tag} ,\n 群: ${message.guild.name} ,\n 頻道: ${message.channel.name} ${hasContent ? `,\n 内容： ${message.content}` : ''} ,\n embed:`;
+    embed(str, channel, receivedEmbed);
+}
   // if it was a reply
   else if (message.reference?.messageId) {
     tries++;
     const repliedTo = await message.channel.messages.fetch(message.reference.messageId);
-    var str = `前文(?: ${repliedTo.author.tag}\n 内容：${repliedTo.content} \n ======================================== \n 人:${message.author.tag} , 訊息: ${message.content} ,\n 群:${message.guild.name} ,\n 頻道:${message.channel.name}`, type = 5
-    var ostr = str;
-    // if (tries <= 1) {
-    //   await msgtype(repliedTo, channel)
-    // }
-    split(ostr, channel)
-  }
-
+    let type = 5;
+    const str = `前文(?: ${repliedTo.author.tag}\n 内容：${repliedTo.content} \n ======================================== \n 人:${message.author.tag} , 訊息: ${message.content} ,\n 群:${message.guild.name} ,\n 頻道:${message.channel.name}`;
+    split(str, channel);
+}
   // normal message
   else {
-    var str = '人:' + message.author.tag + ',\n 訊息: ' + message.content + ' ,\n 群:' + message.guild.name + ' ,\n 頻道:' + message.channel.name, type = 6
+    var str = `人:${message.author.tag},\n 訊息: ${message.content} ,\n 群:${message.guild.name} ,\n 頻道:${message.channel.name}`, type = 6;
     split(str, channel)
   }
 }
