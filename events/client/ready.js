e3c6cb6b57dd9a92e67900d17530f84927b9444b
Alec Chan <alec3d02@gmail.com>
2023-07-02 01:33:49 +0800

    rushia: ready: turn schedule message and self-role into function

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,69 +1,65 @@
-// Imports the client library
-const client = require('../../index.js')
-const secret = require('../../config.json')
+// Imports the client libraries
+const client = require('../../index.js');
+const secret = require('../../config.json');
 const cron = require('cron');
 const { QuickDB } = require("quick.db");
 const db = new QuickDB({ filePath: "database/group.sqlite" });
 
-// start-up activities
-client.on('ready', async () => {
-  const log = await client.channels.fetch(secret.online_log_channel);
-  console.log(`${client.user.tag} is ready on ${client.guilds.cache.size} servers.`);
-  let str = `${client.user.tag} is ready on ${client.guilds.cache.size} servers.\n\`\`\``;
-  client.guilds.cache.forEach(guild => {
-    console.log(`${guild.name}(${guild.id}), ${guild.memberCount} users, ${guild.roles.cache.size} roles, ${guild.channels.cache.size} channels`)
-    str += `\n${guild.name}(${guild.id}), ${guild.memberCount} users, ${guild.roles.cache.size} roles, ${guild.channels.cache.size} channels`
-    db.set(guild.name, guild.id);
+// Define function
+// Schedule Messages
+function startScheduledMessage(time, server, channelid, contents) {
+  let scheduledMessage = new cron.CronJob(time, () => {
+    const guild = client.guilds.cache.get(server);
+    const channel = guild.channels.cache.get(channelid);
+    channel.send(contents);
   });
-  log.send(`**${client.user.tag} was now online!** \n<t:${Math.floor(Date.now() / 1000)}>\n\n${str}\`\`\``);
-
-  // Set bot status
-  client.user.setPresence({ activities: [{ name: "誰在做夢", type: 3 }], status: 'idle', clientStatus: "PS5" });
-
-  // Start scheduled message
-  let scheduledMessage = new cron.CronJob('00 00 12 * * *', () => {
-    const guild = client.guilds.cache.get(secret.grp2);
-    const channel = guild.channels.cache.get(secret.channelID2);
-    channel.send('你各位別當死魚堆');
-  });
-  scheduledMessage.start()
-
-  // Self-role
+  scheduledMessage.start();
+}
+// Self-role
+async function handleSelfRole(guildid, channel, messageid, reaction) {
   const sr_log = await client.channels.fetch(secret.self_role_log_channel);
-  // Get your guild (server) and the specific channel
-  const guild = client.guilds.cache.get('949153367609987124');
-  const channel = guild.channels.cache.get('963802334482284595');
-
-  // Fetch the specific message by ID
-  const message = await channel.messages.fetch('963802394045583370');
-
-  // React to the message with your emoji
-  message.react('<:makaneko_surprise:958407417559908382>');
-
-  // Create a reaction collector
-  const filter = (reaction, user) => {
-    // Check if the emoji name is the one you specified and ignore the bot's reactions
-    return reaction.emoji.name === 'makaneko_surprise' && !user.bot;
-  };
-
-  // Check for reaction changes
-  const collector = message.createReactionCollector({ filter });
-
+  const guild = client.guilds.cache.get(guildid);
+  const message = await guild.channels.cache.get(channel).messages.fetch(messageid);
+  message.react(reaction);
+  const reaction_name = reaction.split("<:")[1].split(":")[0];
+  const collector = message.createReactionCollector({ 
+    filter: (reaction, user) => reaction.emoji.name === reaction_name && !user.bot 
+  });
   collector.on('collect', async (reaction, user) => {
     const link = `https://discord.com/channels/${reaction.message.guildId}/${reaction.message.channelId}/${reaction.message.id}`;
     const role = guild.roles.cache.get('964140235401355304');
     const member = guild.members.cache.get(user.id);
-
-    // Remove roles if they already have it
     if (member.roles.cache.has(role.id)) {
       await member.roles.remove(role);
       reaction.users.remove(user.id);
       sr_log.send(`**Self-role**\n\n${link}\nRemoved role \`\`${role.name}\`\` from \`\`${user.tag}\`\``);
     } else {
-    // Add roles if they didn't have it
       await member.roles.add(role);
       reaction.users.remove(user.id);
       sr_log.send(`**Self-role**\n\n${link}\nAdded role \`\`${role.name}\`\` to \`\`${user.tag}\`\``);
     }
   });
+}
+
+// Ready
+client.on('ready', async () => {
+  // Rushia is now online!
+  let str = `${client.user.tag} is ready on ${client.guilds.cache.size} servers.`
+  console.log(str);
+  const log = await client.channels.fetch(secret.online_log_channel);
+  str += `\n\`\`\``;
+  client.guilds.cache.forEach(guild => {
+    let channel = `${guild.name}(${guild.id}), ${guild.memberCount} users, ${guild.roles.cache.size} roles, ${guild.channels.cache.size} channels`;
+    console.log(channel);
+    str += `\n${channel}`;
+    db.set(guild.name, guild.id);
+  });
+  // Send to log channel
+  log.send(`**${client.user.tag} was now online!** \n<t:${Math.floor(Date.now() / 1000)}>\n\n${str}\`\`\``);
+  // Set status
+  client.user.setPresence({ activities: [{ name: "誰在做夢", type: 3 }], status: 'idle', clientStatus: "PS5" });
+  // Start schedule messages
+  startScheduledMessage("00 00 12 * * *", secret.grp2, secret.channelID2, "你各位別當死魚堆");
+  // Start to handle self-role
+  await handleSelfRole("949153367609987124", "963802334482284595", "963802394045583370", "<:makaneko_surprise:958407417559908382>");
 });
