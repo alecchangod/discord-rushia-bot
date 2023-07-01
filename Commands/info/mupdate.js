2912f3923aaa1d2857e511c69438a6601134f307
Alec Chan <alec3d02@gmail.com>
2023-07-01 15:07:05 +0800

    rushia: commands: info: mupdate: db ==> roles && add await for database operations

--- Commands/info/mupdate.js
+++ Commands/info/mupdate.js
@@ -1,26 +1,27 @@
 const { QuickDB } = require("quick.db");
-const db = new QuickDB({ filePath: "database/roles.sqlite" });
+const roles = new QuickDB({ filePath: "database/members.sqlite" });
 
 module.exports = {
   name: "mupdate",
   aliases: ["m"],
   description: 'log roles changes',
   run: async (client, oldMember, newMember, trans, langc) => {
-    if (!db.get(newMember.user.id)) {
+    const hasRecord = await roles.get(`${newMember.guild.id}_${newMember.user.id}`);
+    if (!hasRecord) {
       newMember.roles.cache.forEach(async role => {
-        await db.push(newMember.user.id, role.id);
+        await roles.push(`${newMember.guild.id}_${newMember.user.id}`, role.id);
       });
     }
 
     const log = newMember.guild.channels.cache.find(ch => ch.name.toLowerCase() === 'log');
-    if (!log) return;
 
     const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
     if (removedRoles.size) {
       const roleNames = removedRoles.map(r => r.name).toString();
       if (roleNames.length) {
-        db.pull(newMember.user.id, removedRoles.map(r => r.id));
+        await roles.pull(`${newMember.guild.id}_${newMember.user.id}`, removedRoles.map(r => r.id));
         const newr = `${oldMember.displayName} 不再是 ${roleNames} 了`;
+        if (!log) return;
         log.send(`身份組變了欸~\n\n${newr}`);
       }
     }
@@ -29,8 +30,9 @@ module.exports = {
     if (addedRoles.size) {
       const roleNames = addedRoles.map(r => r.name).toString();
       if (roleNames.length) {
-        db.push(newMember.user.id, addedRoles.map(r => r.id));
+        await roles.push(`${newMember.guild.id}_${newMember.user.id}`, addedRoles.map(r => r.id));
         const rmr = `${oldMember.displayName} 現在是 ${roleNames} 了`;
+        if (!log) return;
         log.send(`身份組變了欸~\n\n${rmr}`);
       }
     }
