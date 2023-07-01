4196fd950309038827cfc7a679171520594d5a7a
Alec Chan <alec3d02@gmail.com>
2023-07-01 17:59:22 +0800

    rushia: commands: mjs: ss: full re-write

--- Commands/mjs/cmd/ss.mjs
+++ Commands/mjs/cmd/ss.mjs
@@ -1,17 +1,29 @@
 import captureWebsite from 'capture-website';
+
 export const name = "ss";
 export const aliases = ["ss"];
 export const description = 'screenshot website';
+
 export async function run(client, message, args, secret, prefix, trans) {
-    var url = message?.content?.split(" ")[1] || args;
+    let url = message?.content?.split(" ")[1] || args;
+
     if(!url.startsWith("http")) {
         console.log(`ss: Not start with http! ${url}`);
-        var url = `https://${url}`
+        url = `https://${url}`;
     }
-    // if(message?.content?.split(" ")[2] === "full" || args) var tf = true;
-    // else var tf = false;
+
     await captureWebsite.file(url, 'screenshot/screenshot.png', {
-    darkMode: true, width: 2400, height: 1080, fullPage: true, overwrite: true
-});
-    if(message?.channel?.id) message.channel.send({content: `Screenshot of ${url}`, files: ['screenshot/screenshot.png']})
+        darkMode: true, 
+        width: 2400, 
+        height: 1080, 
+        fullPage: true, 
+        overwrite: true
+    });
+
+    if(message?.channel?.id) {
+        message.channel.send({
+            content: `Screenshot of \`\`${url}\`\``, 
+            files: ['screenshot/screenshot.png']
+        });
+    }
 }
