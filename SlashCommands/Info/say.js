df7200c2ed15163e3918c233b46bb8c61d2ed8d6
Alec Chan <alec3d02@gmail.com>
2023-07-02 19:40:51 +0800

    rushia: slash: say: rewrite'

--- SlashCommands/Info/say.js
+++ SlashCommands/Info/say.js
@@ -1,43 +1,84 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, SelectMenuBuilder } = require("@discordjs/builders")
-const { ApplicationCommandOptionType, ButtonStyle } = require("discord.js")
-const { PermissionFlagsBits } = require('discord-api-types/v10');
-
+const { PermissionsBitField, ApplicationCommandOptionType } = require('discord.js');
 module.exports = {
-  name: "say",
-  description: "send message as bot",
-  options: [
-    {
-      name: 'content',
-      description: 'enter message that you want to send',
-      type: ApplicationCommandOptionType.String,
-      require: true
-    },
-    {
-      name: 'id',
-      description: 'enter message id that you want to reply',
-      type: ApplicationCommandOptionType.String,
-      require: false
-    }
-  ],
-  userPermissions: PermissionFlagsBits.ManageMessages,
-  run: async (client, interaction, args, secret, trans, langc, guild) => {
+  data: {
+    name: "say",
+    description: 'send message as bot(approved user only for now)',
+    options: [
+      {
+        name: 'message',
+        type: ApplicationCommandOptionType.Subcommand,
+        description: 'Send a message',
+        options: [
+          {
+            name: 'content',
+            type: ApplicationCommandOptionType.String,
+            description: 'The content of the message',
+            required: true,
+          },
+        ],
+      },
+      {
+        name: 'reply',
+        type: ApplicationCommandOptionType.Subcommand,
+        description: 'Reply to a message',
+        options: [
+          {
+            name: 'messageid',
+            type: ApplicationCommandOptionType.String,
+            description: 'The ID of the message to reply to',
+            required: true,
+          },
+          {
+            name: 'content',
+            type: ApplicationCommandOptionType.String,
+            description: 'The content of the message',
+            required: true,
+          },
+        ],
+      },
+    ],
+    // userPermissions: PermissionsBitField.Flags.ManageMessages,
+  },
+  async execute(client, interaction, args, secret, trans, langc, guild) {
     try {
-      const content = interaction.options.getString('content');
-      const id = interaction.options.getString('id');
-      client.channels.fetch(interaction.channelId).then(async chid => {
-        if (id) {
-          const msg = await chid.messages.fetch(id);
-          if (!msg) return interaction.reply({ content: `Please provide a valid message id.`, ephemeral: true });
-          msg.reply(content);
-          interaction.reply({ content: `${content} has been sent to <#${interaction.channelId}>`, ephemeral: true });
+      // Allow these roles in server (1) and (2)
+      // const allowedRolesGrp1 = ["test", "元首", "管理員", "神志不清的天才寶特瓶"]; // rushia chin server
+      // const allowedRolesGrp2 = ["大哥", "管理員"]; // bot testing group
+      const allowedUser = [secret.me];
+
+      // Check if user was allowed to run this command. If not, their interaction will be ignored.
+      let isAllowed = allowedUser.includes(interaction.member.id)
+      // let isAllowedGrp1 = interaction.guild.id === secret.grp1 && interaction.member.roles.cache.some(role => allowedRolesGrp1.includes(role.name));
+      // let isAllowedGrp2 = interaction.guild.id === secret.grp2 && interaction.member.roles.cache.some(role => allowedRolesGrp2.includes(role.name));
+
+      // If they were allowed
+      if (isAllowed) {
+        // Get message to send
+        const content = interaction.options.getString('content');
+        const messageId = interaction.options.getSubcommand() === 'reply' ? interaction.options.getString('messageid') : null;
+
+        console.log("allowed");
+
+        // Check if the user was replying to another message
+        // If they were, send the message as a reply
+        if (messageId) {
+          const guild = client.guilds.cache.get(interaction.guildId);
+          const message = await guild.channels.cache.get(interaction.channelId).messages.fetch(messageId);
+          if (message) {
+            message.reply(content);
+            interaction.reply({ content: `${content} has been sent to <#${interaction.channelId}>`, ephemeral: true });
+          }
+          else return interaction.reply({ content: `Please provide a valid message id.`, ephemeral: true });
         }
+        // If not, send the message normally 
         else {
-          chid.send(content);
+          interaction.channel.send(content);
           interaction.reply({ content: `${content} has been sent to <#${interaction.channelId}>`, ephemeral: true });
         }
-      })
-    } catch (e) {
-      console.log(e)
+      }
+      else return interaction.reply("笑死你沒權限");
+    } catch (error) {
+      console.log(`Error running say command: ${error}`);
     }
-  }
-}
+  },
+};
