10b7df454c2b60768adb5840fdcc1b230520eb92
Alec Chan <alec3d02@gmail.com>
2023-07-03 13:32:58 +0800

    rushia: slash: spam: rewrite

--- SlashCommands/Info/spam.js
+++ SlashCommands/Info/spam.js
@@ -4,38 +4,37 @@ const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
   data: {
-  name: "spam",
-  description: "spam",
-  required: true,
-  options: [
-    {
-      name: 'content',
-      description: 'enter message that you want to send',
-      type: ApplicationCommandOptionType.String,
-      require: true
-    },
-    {
-      name: 'times',
-      description: 'enter times of message to send',
-      type: ApplicationCommandOptionType.Number,
-      require: false
-    }
-  ],
-},
+    name: "spam",
+    description: "spam message in a channel (owner only for now)",
+    options: [
+      {
+        name: 'content',
+        description: 'Enter message that you want to send',
+        type: ApplicationCommandOptionType.String,
+        required: true
+      },
+      {
+        name: 'amount',
+        description: 'Enter amount of messages to send',
+        type: ApplicationCommandOptionType.Integer,
+        required: true
+      }
+    ]
+  },
+  userPermissions: PermissionsBitField.Flags.ManageMessages,
   async execute(client, interaction, args, secret, trans, langc, guild) {
-    // Parse Amount
-    var usr = interaction.member;
-    if (usr.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
-      let amount = interaction.options.getNumber('times');
+    if (interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+      const amount = interaction.options.getInteger('amount');
       const content = interaction.options.getString('content');
-      interaction.reply({ content: `now sending ${amount} * ${content} to <#${interaction.channelId}>`, ephemeral: true });
-      client.channels.fetch(interaction.channelId).then(async chid => {
-        for (let i = 0; i < amount; i++) {
-          chid.send(content)
-          await wait(100);
-        }
-      })
+
+      interaction.reply({ content: `Now sending ${amount} * ${content} to <#${interaction.channelId}>`, ephemeral: true });
+
+      for (let i = 0; i < amount; i++) {
+        await interaction.channel.send(content);
+        await wait(100);
+      }
+    } else {
+      await interaction.reply("You don't have the required permission.");
     }
-    else interaction.reply("笑死你沒權限")
-  }
-}
+  },
+};
