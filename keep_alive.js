7679cc94d36e8fdaf223b9b261c8c56eccab15cb
Alec Chan <alec3d02@gmail.com>
2023-07-06 14:32:23 +0800

    rushia: re-write website **PLEASE CONTACT ME FOR MENTIONING IN CREDIT AS I CAN'T FIND THE AUTHOR OF THIS IMAGE AT WHERE I DOWNLOADED**

--- keep_alive.js
+++ keep_alive.js
@@ -1,20 +1,43 @@
+//Middleware
 var express = require('express');
 var app = express();
-//Middleware
+var path = require('path');
 
-app.get('/',function(req, res) {
-    var content = "rushia及機器人簡介： \n Uruha Rushia是hololive三期生, 此機器人是參考露來寫的（？）\n \n 雖然感覺只有抖S的部分（X \n \n end(X ";
-
-    res.charset = 'utf-8';
-    res.contentType('text');
-    res.send(content);
+// Middleware to set charset and content type
+app.use((req, res, next) => {
+  res.charset = 'utf-8';
+  res.setHeader('Content-Type', 'text/html');
+  next();
 });
 
+// <p>雖然感覺只有抖S的部分（X</p>
+
+app.get('/', function (req, res) {
+  var content = `
+    <h1>rushia及機器人簡介：</h1>
+    <p>Uruha Rushia是hololive三期生, 此機器人是參考露來寫的（？）</p>
+    <p>然後現在還是...嗎(?</p>
+    <p>完w(X</p><p>
+    <a href="https://github.com/alecchangod/discord-rushia-bot">
+        <img src="images/github.png" width="100" height="100" alt="GitHub">
+    </a>
+    <a href="https://zh.wikipedia.org/zh-tw/潤羽露西婭">
+        <img src="images/wikipedia.png" width="100" height="100" alt="Wikipedia">
+    </a>
+  </p>
+    <img src="images/rushia.png" alt="Rushia" width="750" height="1069">
+  `;
+
+  res.send(content);
+});
+
+// Static file middleware for serving images
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 const ip = require('ip');
 const port = 3000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
-  console.log(`Network access via: localhost:${port}!`);
+  console.log(`Network access via: ${ip.address()}:${port}!`);
 });
