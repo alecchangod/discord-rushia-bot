6ac9e33f46077e701a3aa9df8e05a84352f2bc59
Alec Chan <alec3d02@gmail.com>
2023-07-31 17:25:46 +0800

    rushia: cmd: info: member-update: fix no tag being saved in database and increased logging a bit

--- Commands/info/member-update.js
+++ Commands/info/member-update.js
@@ -5,19 +5,28 @@ module.exports = {
   name: "Member Updates",
   aliases: ["member-update"],
   description: 'log member changes',
-  run: async (client, oldMember, newMember, trans, langc) => {
+  run: async (client, oldMember, newMember, secret, trans, langc) => {
     // Check if was saved at database
     const rolehasRecord = await member.get(`${newMember.guild.id}_roles_${newMember.user.id}`);
     const namehasRecord = await member.get(`${newMember.guild.id}_${newMember.user.id}`);
+
+    // Few logging channels
+    const newrecord = await client.channels.fetch(secret.newrecord_log_channel)
+    const rolelog = await client.channels.fetch(secret.role_log_channel)
+    const usernamelog = await client.channels.fetch(secret.username_log_channel)
+
     // Save if not yet saved
     if ((!rolehasRecord) && (newMember.guild.id)) {
+      newrecord.send(`${newMember.guild.name}(${newMember.guild.id})\n ${newMember.user.tag} adding to record`);
       newMember.roles.cache.forEach(async role => {
         if (((!rolehasRecord) || (!JSON.stringify(rolehasRecord).includes(role.id)))) {
           (async () => {
             await member.push(`${newMember.guild.id}_roles_${newMember.user.id}`, role.id);
+            newrecord.send(`${newMember.guild.name}(${newMember.guild.id})\n ${newMember.user.tag} added to ${role.name}`);
           })();
         }
       });
+      newrecord.send(`${newMember.guild.name}(${newMember.guild.id})\n ${newMember.user.tag} added to record`);
     }
     // Find log channel
     const log = newMember.guild.channels.cache.find(ch => ch.name.toLowerCase() === 'log');
@@ -28,7 +37,8 @@ module.exports = {
       const roleNames = removedRoles.map(r => r.name).toString();
       if (roleNames.length) {
         await member.pull(`${newMember.guild.id}_roles_${newMember.user.id}`, removedRoles.map(r => r.id));
-        const newr = `${oldMember.displayName} 不再是 ${roleNames} 了`;
+        const newr = `${newMember.user.tag} 不再是 ${roleNames} 了`;
+        rolelog.send(`${newMember.guild.name}(${newMember.guild.id})\n ${newr}`)
         if (log) log.send(`**身份組變了欸~**\n\n${newr}`);
       }
     }
@@ -38,7 +48,8 @@ module.exports = {
       const roleNames = addedRoles.map(r => r.name).toString();
       if (roleNames.length) {
         await member.push(`${newMember.guild.id}_roles_${newMember.user.id}`, addedRoles.map(r => r.id));
-        const rmr = `${oldMember.displayName} 現在是 ${roleNames} 了`;
+        const rmr = `${newMember.user.tag} 現在是 ${roleNames} 了`;
+        rolelog.send(`${newMember.guild.name}(${newMember.guild.id})\n ${rmr}`)
         if (log) log.send(`**身份組變了欸~**\n\n${rmr}`);
       }
     }
@@ -51,16 +62,22 @@ module.exports = {
       })();
       const name = `${namehasRecord} 現在是 ${newMember.user.tag} 了`;
       // Only send message when really changed name
-      if ((log) && (namehasRecord != newMember.user.tag)) log.send(`**Changed username**\n\n${name}`)
+      if ((namehasRecord != newMember.user.tag)) {
+        usernamelog.send(`${newMember.guild.name}(${newMember.guild.id})\n ${name}`)
+        if (log) log.send(`**Changed username**\n\n${name}`)
+      }
     }
     // Display name changes
-    if ((newMember.displayName) || (namehasRecord != newMember.displayName)) {
+    if ((newMember.displayName) || (namehasRecord != `${newMember.displayName}#${newMember.user.discriminator}`)) {
       (async () => {
-        await member.set(`${newMember.guild.id}_${newMember.user.id}`, newMember.displayName);
+        await member.set(`${newMember.guild.id}_${newMember.user.id}`, `${newMember.displayName}#${newMember.user.discriminator}`);
       })();
-      const name = `${namehasRecord} 現在是 ${newMember.displayName} 了`;
+      const name = `${namehasRecord} 現在是 ${newMember.displayName}#${newMember.user.discriminator} 了`;
       // Only send message when really changed name
-      if ((log) && (namehasRecord != newMember.displayName)) log.send(`**Changed username**\n\n${name}`)
+      if ((namehasRecord != `${newMember.displayName}#${newMember.user.discriminator}`)) {
+        usernamelog.send(`${newMember.guild.name}(${newMember.guild.id})\n ${name}`)
+        if (log) log.send(`**Changed username**\n\n${name}`)
+      }
     }
   }
 }
